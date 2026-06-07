99036a2aa322292ec3ab6fac37a80a73ed17c0d5
Aswin1310 <aswinjaba12345@gmail.com>
2026-06-07 15:39:24 +0530

    fix admin login

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-admin-key');
 
     if (req.method === 'OPTIONS') {
       return res.sendStatus(204);
